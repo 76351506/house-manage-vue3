5406d1126be60d5c3ff1dfaa736abc423876beed
heinan <76351506@163.com>
2023-07-17 15:47:07 +0800

    提交404、500

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>this is layout </div>
+  <div>this is layout</div>
 </template>
-<script lang="ts" setup>
-    
-</script>
+<script lang="ts" setup></script>
 <style lang="less"></style>
--- src/views/user/registry/index.vue
+++ src/views/user/registry/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>this is  registry page</div>
+  <div>this is registry page</div>
 </template>
-<script lang="ts" setup>
-    
-</script>
+<script lang="ts" setup></script>
 <style lang="less"></style>
